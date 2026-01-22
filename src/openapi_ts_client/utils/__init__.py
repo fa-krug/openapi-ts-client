b7111f4d9a331487c10c037d3692bc50b31865c0
Sascha Krug <skrug@avodaq.com>
2026-01-22 16:55:43 +0100

    chore: export utils functions from package

--- src/openapi_ts_client/utils/__init__.py
+++ src/openapi_ts_client/utils/__init__.py
@@ -1,1 +1,19 @@
 """Shared utilities for OpenAPI TypeScript client generation."""
+
+from .naming import (
+    TYPESCRIPT_RESERVED_WORDS,
+    operation_id_to_method_name,
+    schema_to_filename,
+    tag_to_service_filename,
+    tag_to_service_name,
+)
+from .openapi import load_and_resolve_spec
+
+__all__ = [
+    "TYPESCRIPT_RESERVED_WORDS",
+    "operation_id_to_method_name",
+    "schema_to_filename",
+    "tag_to_service_filename",
+    "tag_to_service_name",
+    "load_and_resolve_spec",
+]
